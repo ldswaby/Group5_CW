8539a528ee7567ac6e50422932ff3a02a2e525b1
Kyle-J-Sun <S.Jingkai@Outlook.com>
2020-12-20 19:50:58 +0800

    All week3 groupworks

--- Week3/Code/get_TreeHeight.py
+++ Week3/Code/get_TreeHeight.py
@@ -4,4 +4,34 @@
 # Script Name: get_TreeHeight.py
 # Author: Jingkai Sun (ks3020@ic.ac.uk)
 
+import sys
+import pandas as pd
+import numpy as np
+import re
+
+def TreesHeight(degrees, dist):
+    radians = degrees * np.pi / 180
+    height = dist * np.tan(radians)
+    print(height)
+
+    return(height)
+
+def main(argv):
+    default_path = "../Data/"
+    filename = argv[1]
+    filename_noExt = re.search(r"\w+", filename).group()
+    save_name = "../Results/%s_treeheights_python.csv" % filename_noExt
+    filepath = default_path + filename
+    trees_data = pd.DataFrame(pd.read_csv(filepath))
+    
+    trees_data["Height"] = TreesHeight(trees_data["Angle.degrees"], trees_data["Distance.m"])
+    
+    # Save to csv
+    trees_data.to_csv(save_name, sep=",", index = False)
+    return 0
+
+if __name__ == "__main__":
+    status = main(sys.argv)
+    sys.exit(status)
+
 
--- Week3/Code/run_get_TreeHeight.sh
+++ Week3/Code/run_get_TreeHeight.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-echo "Run get_TreeHeight.R with an argument 'Trees.csv'"
+echo "Run get_TreeHeight.R and get_TreeHeight.py with an argument 'Trees.csv'"
 Rscript get_TreeHeight.R Trees.csv
+python3 get_TreeHeight.py Trees.csv
+echo "Finished!"
